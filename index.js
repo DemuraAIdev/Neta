1ca1e9f8aee4a2c05468ee3a8adc96ac5b1c2fbe
DemuraAIdev <cyberaioff@gmail.com>
2025-06-21 15:58:19 +0700

    chore: Enable SSL for Minio client connection

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const upload = multer({ storage });
 const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
   port: parseInt(process.env.MINIO_PORT, 10),
-  useSSL: false,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
